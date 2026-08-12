432f9d0649ae5dab83ed9fe975c5a4632cab8bd4
dhanus0416 <dhanushkanchanapalli04@gmail.com>
2026-08-12 17:38:30 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
-
-#include <stdlib.h>
-#include <stdio.h>
-
-int main() {
+#include<stdio.h>
+int main(){
+    char letter ='A';
     
-    printf("Hello, World!\n");
+    int count = 25;
+    
+    float price = 19.99f;
+    
+    double pi = 3.14365773777883;
+    
+    printf("char :%c | int: %d | float: %.2f |double: %.101f", letter,count,price,pi);
     
 }
